25d8cb7888a15b87e37c8bb2843452457446b359
gharseldin <gharseldin@aucegypt.edu>
2021-01-24 12:12:51 -0800

    Initial Commit

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by Amr Gharseldin on 1/24/21.
+//  Copyright © 2021 Gharseldin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
