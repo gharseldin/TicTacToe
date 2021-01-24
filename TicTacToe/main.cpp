acb59d93b09262845fabd271f345fee48349b397
Ahmed Gharseldin <A.Ghars@outlook.com>
2021-01-25 01:26:57 +0400

    moving the comment above the header

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -6,7 +6,8 @@
 //  Copyright © 2021 Gharseldin. All rights reserved.
 //
 
-#include <bits/stdc++.h> // lets include this cool header
+// lets include this cool header
+#include <bits/stdc++.h>
 
 // and this as well to remove the std which I have no idea what it does or what it is however those double dots std::cout remind me of the classes and OOP
 using namespace std;
